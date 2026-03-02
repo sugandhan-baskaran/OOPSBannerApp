5c0f4cc208b96e2a1f59afe79bb8f98f5c3f5df2
sugandhan-baskaran <sugandhanbaskaran@gmail.com>
2026-03-02 08:43:06 +0530

    updated the code

--- UC2.java
+++ UC2.java
@@ -1,13 +1,13 @@
 public class UC2 {
-    public static void main(String[] args) {
-
-        System.out.println("  ***   " + "  " + "  ***** ");
-        System.out.println(" **   ** " + "  " + " **      ");
-        System.out.println("**     **" + "  " + "**       ");
-        System.out.println("**     **" + "  " + " *****   ");
-        System.out.println("**     **" + "  " + "     **  ");
-        System.out.println(" **   ** " + "  " + " **   ** ");
-        System.out.println("  ***   " + "  " + "  ***** ");
-
+    public static void main(String[] args){
+        System.out.println("   ***   "+"   "+"   ***   "+"   "+"*****   "+"   "+"   ***** ");
+        System.out.println(" **   ** "+"   "+" **   ** "+"   "+"**   ** "+"   "+" **      ");
+        System.out.println("**     **"+"   "+"**     **"+"   "+"**    **"+"   "+"**       ");
+        System.out.println("**     **"+"   "+"**     **"+"   "+"**   ** "+"   "+" **      ");
+        System.out.println("**     **"+"   "+"**     **"+"   "+"*****   "+"   "+"   ***   ");
+        System.out.println("**     **"+"   "+"**     **"+"   "+"**      "+"   "+"      ** ");
+        System.out.println("**     **"+"   "+"**     **"+"   "+"**      "+"   "+"       **");
+        System.out.println(" **   ** "+"   "+" **   ** "+"   "+"**      "+"   "+"      ** ");
+        System.out.println("   ***   "+"   "+"   ***   "+"   "+"**      "+"   "+" *****   ");
     }
 }
